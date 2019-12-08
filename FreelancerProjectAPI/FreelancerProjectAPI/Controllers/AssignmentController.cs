e62adb3eb7ace9213ec0fccf55b54192a3cc19b5
Jeff <38490982+HeffeV@users.noreply.github.com>
2019-12-08 21:59:50 +0100

    1.0.0

---
+++ FreelancerProjectAPI/FreelancerProjectAPI/Controllers/AssignmentController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FreelancerProjectAPI.Models;
+
+namespace FreelancerProjectAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AssignmentController : ControllerBase
+    {
+        private readonly DatabaseContext _context;
+
+        public AssignmentController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Assignment
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Assignment>>> GetAssignments()
+        {
+            return await _context.Assignments.ToListAsync();
+        }
+
+        // GET: api/Assignment/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Assignment>> GetAssignment(long id)
+        {
+            var assignment = await _context.Assignments.FindAsync(id);
+
+            if (assignment == null)
+            {
+                return NotFound();
+            }
+
+            return assignment;
+        }
+
+        // PUT: api/Assignment/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAssignment(long id, Assignment assignment)
+        {
+            if (id != assignment.AssignmentID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(assignment).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AssignmentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Assignment
+        [HttpPost]
+        public async Task<ActionResult<Assignment>> PostAssignment(Assignment assignment)
+        {
+            _context.Assignments.Add(assignment);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAssignment", new { id = assignment.AssignmentID }, assignment);
+        }
+
+        // DELETE: api/Assignment/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Assignment>> DeleteAssignment(long id)
+        {
+            var assignment = await _context.Assignments.FindAsync(id);
+            if (assignment == null)
+            {
+                return NotFound();
+            }
+
+            _context.Assignments.Remove(assignment);
+            await _context.SaveChangesAsync();
+
+            return assignment;
+        }
+
+        private bool AssignmentExists(long id)
+        {
+            return _context.Assignments.Any(e => e.AssignmentID == id);
+        }
+    }
+}
---
+++ FreelancerProjectAPI/FreelancerProjectAPI/Controllers/CompanyController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FreelancerProjectAPI.Models;
+
+namespace FreelancerProjectAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+        private readonly DatabaseContext _context;
+
+        public CompanyController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Company
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Company>>> GetCompanies()
+        {
+            return await _context.Companies.ToListAsync();
+        }
+
+        // GET: api/Company/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Company>> GetCompany(long id)
+        {
+            var company = await _context.Companies.FindAsync(id);
+
+            if (company == null)
+            {
+                return NotFound();
+            }
+
+            return company;
+        }
+
+        // PUT: api/Company/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCompany(long id, Company company)
+        {
+            if (id != company.CompanyID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(company).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CompanyExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Company
+        [HttpPost]
+        public async Task<ActionResult<Company>> PostCompany(Company company)
+        {
+            _context.Companies.Add(company);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCompany", new { id = company.CompanyID }, company);
+        }
+
+        // DELETE: api/Company/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Company>> DeleteCompany(long id)
+        {
+            var company = await _context.Companies.FindAsync(id);
+            if (company == null)
+            {
+                return NotFound();
+            }
+
+            _context.Companies.Remove(company);
+            await _context.SaveChangesAsync();
+
+            return company;
+        }
+
+        private bool CompanyExists(long id)
+        {
+            return _context.Companies.Any(e => e.CompanyID == id);
+        }
+    }
+}
