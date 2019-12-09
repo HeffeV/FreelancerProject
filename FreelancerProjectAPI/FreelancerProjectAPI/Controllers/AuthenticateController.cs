26c292aac59470df0a83ea4d15229600b2f49bab
Jeff <38490982+HeffeV@users.noreply.github.com>
2019-12-09 08:43:29 +0100

    1.0.0

---
+++ FreelancerProjectAPI/FreelancerProjectAPI/Controllers/AuthenticateController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FreelancerProjectAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreelancerProjectAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticateController : ControllerBase
+    {
+        private IUserService _userService;
+        public AuthenticateController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody]Models.User userParam)
+        {
+            //authenticate user
+            var user = _userService.Authenticate(userParam.Email, userParam.Password);
+            if (user == null) return BadRequest(new { message = "Email or password is incorrect" });
+            return Ok(user);
+        }
+    }
+}
