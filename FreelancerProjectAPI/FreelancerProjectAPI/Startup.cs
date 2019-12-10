c3e69bf957cce306b96b0a5b9496a77c2ade7456
Jeff <38490982+HeffeV@users.noreply.github.com>
2019-12-10 17:50:11 +0100

    Update Startup.cs

--- FreelancerProjectAPI/FreelancerProjectAPI/Startup.cs
+++ FreelancerProjectAPI/FreelancerProjectAPI/Startup.cs
@@ -72,7 +72,7 @@ namespace FreelancerProjectAPI
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseSwagger(); app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "Member API v1"); });
+            app.UseSwagger(); app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "Freelancer API"); });
 
             app.UseCors("MyPolicy");
             app.UseHttpsRedirection();
