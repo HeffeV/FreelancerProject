73f952e340686f3b5eec34812742be3b6512ff05
AliKiyak <r0702374@student.thomasmore.be>
2019-12-12 17:06:44 +0100

    check if reviewed toegevoegd

--- FreelancerProjectAPI/FreelancerProjectAPI/Controllers/ReviewController.cs
+++ FreelancerProjectAPI/FreelancerProjectAPI/Controllers/ReviewController.cs
@@ -10,7 +10,6 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace FreelancerProjectAPI.Controllers
 {
-    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ReviewController : ControllerBase
@@ -27,6 +26,26 @@ namespace FreelancerProjectAPI.Controllers
         public async Task<ActionResult<IEnumerable<Review>>> GetReviews()
         {
             return await _context.Reviews.ToListAsync();
+        }
+
+        [HttpGet]
+        [Route("checkifuserreviewedcompany/{companyid}")]
+        public Boolean checkIfUserReviewedCompany(long companyid)
+        {
+            //var userid = long.Parse(this.User.Claims.First(i => i.Type == "UserID").Value);
+            long userid = 2;
+            User user = _context.Users.Find(userid);
+            var company = _context.Companies
+                .Include(c => c.Reviews).ThenInclude(r=> r.User)
+                .FirstOrDefault(c => c.CompanyID == companyid);
+            var testobject = company.Reviews.Any(r => r.User.UserID == userid);
+            if (testobject)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
         }
 
         // GET: api/Review/5
@@ -77,8 +96,8 @@ namespace FreelancerProjectAPI.Controllers
         [Route("addreviewtocompany")]
         public ActionResult<Review> PostReview(Review review)
         {
-            var userid = long.Parse(this.User.Claims.First(i => i.Type == "UserID").Value);
-            //long userid = 1;
+            //var userid = long.Parse(this.User.Claims.First(i => i.Type == "UserID").Value);
+            long userid = 1;
             User user = _context.Users.Find(userid);
             var company = _context.Companies.FirstOrDefault(c => c.CompanyID == review.Company.CompanyID);
             review.User = user;
